06a25d0defe734a9ac313e23bfbd8e0b4c1e4775
swdev.smz <swdev.smz@gmail.com>
2019-03-27 22:47:57 +0900

    プログラムでフォーカス制御した場合のタッチキーボードの挙動を調査

---
+++ TouchKeySample/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TouchKeySample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Focus();
+            textBox1.Focus();
+            textBox2.Focus();
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            Console.WriteLine("Focus");
+        }
+    }
+}
